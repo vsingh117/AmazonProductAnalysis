29c141d893c59f1143d2ffa02218f1a9645d6757
Vijay Singh Chauhan <49814688+vsingh117@users.noreply.github.com>
2020-11-23 11:21:13 -0500

    Update product_price.java

--- product_price.java
+++ product_price.java
@@ -153,8 +153,8 @@ public class product_price extends Configured implements Tool {
 								    avg_price = total / 2;
 								}
 								
-								double bucket = Math.floor(avg_price/50.0)*50.0;
-								String bucketText = bucket + " to " + (bucket + 50.0);
+								double bucket = Math.floor(avg_price/2.0)*2.0;
+								String bucketText = bucket + " to " + (bucket + 2.0);
 								
 								context.write(new Text(bucketText),one);
 								
